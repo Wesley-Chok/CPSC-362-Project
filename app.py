be5f03a4a48140e7c406e475a978e64c1725d70d
Wesley-Chok <55215498+Wesley-Chok@users.noreply.github.com>
2023-02-22 11:27:15 -0800

    Added a home , search and recipe function
    Includes routes as well

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
 
-app = Flask(__name__)
+API_URL = 'https://www.themealdb.com/api/json/v1/1/'
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+@app.route('/search')
+def search():
+    query = request.args.get('query')
+    if query:
+        response = requests.get(f'{API_URL}search.php?s={query}')
+        if response.ok:
+            results = response.json().get('meals')
+            logging.info(f'Successfully retrieved {len(results)} search results for query "{query}"')
+        else:
+            results = None
+            error_message = f"Error: {response.status_code} - {response.reason}"
+            logging.error(f'API call failed with error message: {error_message}')
+            return render_template('search.html', query=query, results=results, error_message=error_message)
+    else:
+        results = None
+    logging.info(f'Rendering search results template for query "{query}"')
+    return render_template('search.html', query=query, results=results)
+
+@app.route('/recipe/<id>')
+def recipe(id):
+    response = requests.get(f'{API_URL}lookup.php?i={id}')
+    recipe = response.json().get('meals')[0]
+    return render_template('recipe.html', recipe=recipe)
